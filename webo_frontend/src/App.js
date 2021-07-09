456d495ea9bb1b7c4e767415f98fe0a401c21227
JesseNeon <62482001+JesseAyegba@users.noreply.github.com>
2021-07-09 20:43:07 +0100

    React router optimizations

--- webo_frontend/src/App.js
+++ webo_frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import General from './Pages/General/General';
-import Analysis from './Pages/Analysis/Analysis';
-import Visualization from './Pages/Visualization/Visualization';
+import logo from "./logo.svg";
+import "./App.css";
+import General from "./Pages/General/General";
+import Analysis from "./Pages/Analysis/Analysis";
+import Visualization from "./Pages/Visualization/Visualization";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
+    <Router>
       <Switch>
-      <div className="app-component">
-                <Route path="/" component={General} exact />
-                <Route path="/analysis" component={Analysis} />
-                <Route path="/visualization" component={Visualization} />
-            
-      </div>
+        <Route exact path="/" component={General} exact />
+        <Route exact path="/analysis" component={Analysis} />
+        <Route exact path="/visualization" component={Visualization} />
       </Switch>
-    </div>
+    </Router>
   );
 }
 
